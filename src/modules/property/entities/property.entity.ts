c22d5e96ad203eea8f8cdb0382152faf2a98424e
Intuneteq <tobiolanitori@gmail.com>
2023-07-11 04:52:23 +0100

    chore: code review for seun

--- src/modules/property/entities/property.entity.ts
+++ src/modules/property/entities/property.entity.ts
@@ -32,7 +32,9 @@ export class Property {
 
   @Column({ type: 'integer', nullable: true })
   price: number | null;
-
+/**
+ * Tags are supposed to be dynamic and not fixed so intead of this have a features array where the admin
+ * can speciy landSize instead
   @Column({ type: 'text', nullable: true })
   landSize: string | null;
 
@@ -41,13 +43,17 @@ export class Property {
 
   @Column({ type: 'integer', nullable: true })
   numberOfBedroom: number | null;
-
+*/
   @OneToMany(() => Comment, (comment) => comment.propertyId)
   comments: Comment[];
 
   @ManyToOne(() => User, (user) => user.properties)
-  user: User;
+  user: User; // rename user to admin here so it will be ref as adminId in the db
 
+  /**
+   * This is not exactly intuitive because only one of these can be true at a time
+   * Have a property status instead where the user can update depending on the current
+   * status of the property
   @Column({ type: 'boolean', default: false })
   isSold: boolean;
 
@@ -56,6 +62,8 @@ export class Property {
 
   @Column({ type: 'boolean', default: false })
   unlisted: boolean;
+
+   */
 
   @Column({ type: 'float', nullable: true })
   propertyRating: number | null;
@@ -78,3 +86,62 @@ export class Property {
   @Column({ type: 'varchar', length: 255, nullable: true })
   video: string | null;
 }
+
+
+// manytoone rel with admin 
+
+// title
+
+// description
+
+// location
+
+// price
+
+// property type (should be house and land)
+
+// upload images and video to cloudinary and save their url
+
+// tags array of strings
+
+// features: array of strings
+
+//property status (listed, unlisted, deleted and sold. unlisted should be the default status)
+
+// one to many rel with reviews
+
+//salesSuportName
+
+//salesSuportNum
+
+//salesSupportAvatar
+
+// timestamps
+
+
+// Entity reviews
+
+// many to one rel with property
+
+// property (the score; min is 1, max is 5)
+
+//valueForMoney (the score; min is 1, max is 5)
+
+//location (the score; min is 1, max is 5)
+
+// support (the score; min is 1, max is 5)
+
+// name (name of reviewer)
+
+// email
+
+// review (the review itself)
+
+// reviewScore (the average of all above scores)
+
+//timestamps
+
+
+
+
+
