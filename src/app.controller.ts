45ef4df49c76c3f87adbf263bfaf247e2e53b6c8
Taiwola <seunolanitori@gmail.com>
2023-07-28 09:16:10 +0100

    routes changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('app')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('home')
+  @Get()
   @Render('index')
   root() {
     return;
